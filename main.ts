1be3a245153dd6491041203741054c7bf47b3b29
Charlotte-techniek <charlotte.malfliet99@gmail.com>
2024-11-12 10:21:22 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, maqueen.Ultrasonic(PingUnit.Centimeters))
+for (let index = 0; index < 4; index++) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 38)
+    basic.pause(2000)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 44)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 44)
+    basic.pause(885)
+}
 maqueen.motorStop(maqueen.Motors.All)
 basic.forever(function () {
 	
